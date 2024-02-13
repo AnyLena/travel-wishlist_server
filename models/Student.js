f5a5bc9876b8ec7320422acc532d60d05049386a
LenaBultmann <lena.bultmann@gmx.de>
2024-02-13 13:00:07 +0100

    minor changes

--- models/Student.js
+++ models/Student.js
@@ -2,19 +2,25 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 const StudentSchema = new mongoose.Schema({
-  name: { required: true, type: String },
-  first_name: { required: true, type: String },
+  name: { 
+    required: true,
+    type: String },
+  first_name: { 
+    required: true,
+    type: String },
   email: {
     required: true,
     type: String,
     validate: {
-        validator: function(v) {
-          return /\S+@\S+.\S+/.test(v);
-        },
-        message: "not a valid email!"
+      validator: function (v) {
+        return /\S+@\S+.\S+/.test(v);
       },
+      message: "not a valid email!",
+    },
   },
-  country: { required: true, type: Schema.Types.ObjectId, ref: 'Country' }
+  country: { 
+    required: true, 
+    type: mongoose.Schema.Types.ObjectId, ref: "Country" },
 });
 
 const Student = mongoose.model("Student", StudentSchema);
